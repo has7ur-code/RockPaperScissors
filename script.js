d212703dd8c2d719d9081d699893240d6437a775
ViktorB <kerimh94@gmail.com>
2026-06-16 15:39:00 +0200

    added user input and playRound function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello World')
+
+const randomNumber = Math.floor(Math.random() * 3)
+
+if (randomNumber === 0) {
+    console.log('rock');
+} else if (randomNumber === 1) {
+    console.log('paper');
+} else if (randomNumber === 2) {
+    console.log('scissors');
+}
